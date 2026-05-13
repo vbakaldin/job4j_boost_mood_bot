d7d569144b9976f97c63aa3c2e6146159c2be4b8
vladimir <tintutin@list.ru>
2026-05-13 22:50:47 +0400

    aop

--- src/main/java/ru/job4j/logging/LoggingAspect.java
+++ src/main/java/ru/job4j/logging/LoggingAspect.java
@@ -1,4 +1,18 @@
 package ru.job4j.logging;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
 public class LoggingAspect {
+    @Before("execution(* ru.job4j.services.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("Method: " + joinPoint.getSignature().getName());
+        System.out.println("Args: " + Arrays.toString(joinPoint.getArgs()));
+    }
 }
